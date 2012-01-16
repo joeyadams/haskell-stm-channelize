e51b36894ff92948f31cb0a00908c3b57fc7814c
Joey Adams <joeyadams3.14159@gmail.com>
2012-01-15 22:59:32 -0500

    Implemented almost all but the killer thread, and removed unused functions

--- Control/Concurrent/STM/Channelize.hs
+++ Control/Concurrent/STM/Channelize.hs
@@ -15,11 +15,19 @@
 --
 -- TODO: Add support for bounded-tchan.
 
-{-# LANGUAGE CPP, DeriveDataTypeable, ExistentialQuantification #-}
+{-# LANGUAGE CPP, DeriveDataTypeable #-}
 module Control.Concurrent.STM.Channelize (
     Config(..),
     channelize,
     ChannelizeException(..),
+
+    -- * Custom channel types
+    -- | You may want to use a different type of channel than 'TChan' (e.g.
+    -- TBChan in the stm-chans package).  'TChan' places no limit on the queue
+    -- length, so your application may start leaking memory if a client
+    -- produces messages faster than they can be consumed.
+    --
+    -- TODO
 ) where
 
 import Control.Concurrent
@@ -79,34 +87,28 @@ data Config msg_in msg_out
 -- If either the receive callback or send callback encounters an exception, it
 -- will be wrapped in a 'ChannelizeException' and thrown to your thread.
 --
--- It is guaranteed that:
---
---  * The receive callback is only called from thread (1).
---
---  * The send callback is only called from thread (2).
---
---  * Neither callback is called after 'channelize' completes or throws an
---    exception.
-channelize :: IO msg_in             -- ^ Receive callback
-           -> (msg_out -> IO ())    -- ^ Send callback
-           -> IO ()                 -- ^ Close callback
+-- Before returning, 'channelize' will wait for sending and receiving to stop, and for 
+channelize :: Config msg_in msg_out
+                -- ^ Callbacks for sending and receiving on the connection, and
+                -- for closing the connection
            -> (TChan msg_in -> TChan msg_out -> IO a)
-                                    -- ^ Inner computation
+                -- ^ Inner computation
            -> IO a
-channelize recv send close inner = do
+channelize config inner = do
     recv_chan       <- newTChanIO
     send_chan       <- newTChanIO
     stop            <- newTVarIO False
     recv_stopped    <- newTVarIO False
     send_stopped    <- newTVarIO False
-
-    caller_tid <- myThreadId
+    killer_stopped  <- newTVarIO False
+    caller_tid      <- myThreadId
+    caller_gate     <- newGate
 
     let
         -- recvLoop and sendLoop terminate without exception if they detect
         -- that the stop variable is set.
         recvLoop = do
-            msg <- recv
+            msg <- recvMsg config
             checkVar stop (writeTVar recv_stopped True) $ do
                 writeTChan recv_chan msg
                 return recvLoop
@@ -115,18 +117,26 @@ channelize recv send close inner = do
             checkVar stop (writeTVar send_stopped True) $ do
                 msg <- readTChan send_chan
                 return $ do
-                    send msg
+                    sendMsg config msg
                     sendLoop
 
-        recvThread send_tid = do
-            undefined
+        throwToCaller wrapper =
+            whenGateIsOpen caller_gate . throwTo caller_tid . wrapper
 
-        sendThread recv_tid = do
-            undefined
+    recv_tid <- forkIO $ recvLoop `E.catch` throwToCaller RecvError
+    send_tid <- forkIO $ sendLoop `E.catch` throwToCaller SendError
 
-    (recv_tid, send_tid) <- forkIO_pair recvThread sendThread
+    _killer_tid <- forkIO $ do
+        waitFor stop
 
-    undefined
+        undefined
+
+        undefined `finally` (atomically $ writeTVar killer_stopped True)
+
+    inner recv_chan send_chan `finally` do
+        atomically $ writeTVar stop True
+        closeGate caller_gate
+        waitFor killer_stopped
 
 
 ------------------------------------------------------------------------
@@ -167,48 +177,15 @@ closeGate (Gate gate) =
 
 
 ------------------------------------------------------------------------
--- Internal helpers
+-- Miscellaneous helpers
 
-
--- | Spawn two threads, passing them each other's thread IDs.
-forkIO_pair :: (ThreadId -> IO ())
-            -> (ThreadId -> IO ())
-            -> IO (ThreadId, ThreadId)
-forkIO_pair action1 action2 = do
-    tid2_var <- newTVarIO Nothing
-
-    tid1 <- forkIO $ join $ atomically $ do
-        m <- readTVar tid2_var
-        case m of
-            Nothing   -> retry    -- Still waiting for the other thread's ID
-            Just tid2 -> return $ action1 tid2
-
-    tid2 <- forkIO $ action2 tid1
-
-    atomically $ writeTVar tid2_var $ Just tid2
-    return (tid1, tid2)
-
--- | Wait for a thread to set a TVar to True.  If it takes too long, kill it.
-waitThenKill :: TVar Bool -> ThreadId -> IO ()
-waitThenKill response tid = do
-    let delay_length = 1000000
-
-    waitThenDo delay_length response $ do
-        killThread tid
-
-        -- Give the thread a second chance to signal completion
-        waitThenDo delay_length response (return ())
-
--- | Wait for the TVar's value to become True.  If it takes longer than the
--- given number of microseconds, perform the given action (otherwise, don't).
-waitThenDo :: Int -> TVar Bool -> IO () -> IO ()
-waitThenDo timeout var action = do
-    delay <- registerDelay timeout
-    checkVar var (return ()) $ do
-        timed_out <- readTVar delay
-        if timed_out
-            then return action
-            else retry
+-- | Wait for the TVar's value to become 'True'.
+waitFor :: TVar Bool -> IO ()
+waitFor var = atomically $ do
+    v <- readTVar var
+    if v
+        then return ()
+        else retry
 
 -- | If var is True, call on_set.  Otherwise, run or_else followed by the IO
 -- action it returns.
@@ -223,17 +200,3 @@ checkVar var on_set or_else =
                 else retry
         ) `orElse` or_else
 
--- | Like 'E.mask', but backported to base before version 4.3.0.
---
--- Note that the restore callback is monomorphic, unlike in 'E.mask'.  This
--- could be fixed by changing the type signature, but it would require us to
--- enable the RankNTypes extension.  This module doesn't need that
--- polymorphism, anyway.
-portableMask :: ((IO a -> IO a) -> IO b) -> IO b
-#if MIN_VERSION_base(4,3,0)
-portableMask io = E.mask $ \restore -> io restore
-#else
-portableMask io = do
-    b <- E.blocked
-    E.block $ io $ \m -> if b then m else E.unblock m
-#endif
